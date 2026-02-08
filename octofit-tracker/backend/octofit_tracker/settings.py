be09af55dc52bdd2eb0e3ecc14c66c84845d37dd
vidu:)shi <154583551+vidu-125shi@users.noreply.github.com>
2026-02-08 14:32:59 +0000

    setup django

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-c@hvf07=sf26ld*84!24mprzmn=sar(kn4b%nykvbt8mg)qri6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(CODESPACE_NAME)
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
+import os
+from django.http import JsonResponse
 from django.urls import path
+from django.contrib import admin
+
+def api_root(request):
+    codespace_name = os.environ.get('CODESPACE_NAME', '')
+    base_url = f"https://{codespace_name}-8000.app.github.dev" if codespace_name else "http://localhost:8000"
+    return JsonResponse({
+        'activities': f'{base_url}/api/activities/',
+        'users': f'{base_url}/api/users/',
+        'teams': f'{base_url}/api/teams/',
+        'workouts': f'{base_url}/api/workouts/',
+        'leaderboards': f'{base_url}/api/leaderboards/',
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_root),
 ]
